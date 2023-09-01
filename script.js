55c2bde5f75c05efabc81daa0ea7219d13dabc6f
Aaron Williams <polarizedcuriosity@gmail.com>
2023-09-01 15:44:01 -0700

    second commit, added some css and the inputs

---
+++ script.js
@@ -0,0 +1,4 @@
+function weightConverter(valNum) {
+  document.getElementById("outputGrams").innerHTML = valNum / 0.0022046;
+  document.getElementById("outputGrams").innerHTML = valNum / 0.0022046;
+}
