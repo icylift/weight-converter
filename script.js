e1db5ce79e1797fa133ff43ed0c80d22aef364e6
Aaron Williams <polarizedcuriosity@gmail.com>
2023-09-02 16:28:47 -0700

    Changed the javascript to make it more simple, and decide to just display the other measurments instead of using inputs. added another section to  the css to target the paragraphs.

--- script.js
+++ script.js
@@ -1,13 +1,4 @@
-// Calling the Inputs by ID
-let kgRef = document.getElementById("kg");
-let lbsRef = document.getElementById("lbs");
-let ozRef = document.getElementById("oz");
-
-let convertFromKg = () => {
-  let kg = kgRef.value;
-  //toFixed(2) limits the decimals to 2 digits.
-  lbsRef.value = (kg * 2.205).toFixed(2);
-  ozRef.value = (kg * 35.274).toFixed(2);
-};
-
-kgRef.addEventListener("input", convertFromKg);
+function weightConvert(value) {
+  document.getElementById("Kilograms").innerHTML = value * 2.2046;
+  document.getElementById("Ounces").innerHTML = value * 35.274;
+}
