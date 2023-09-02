73eb160ac0fcc092065d5f6b50cc9e7521f913be
Aaron Williams <polarizedcuriosity@gmail.com>
2023-09-02 01:28:17 -0700

    Added the fuction to convert weight, I need to fix the addEventListner the console is saying the its null.

--- script.js
+++ script.js
@@ -1,4 +1,13 @@
-function weightConverter(valNum) {
-  document.getElementById("outputGrams").innerHTML = valNum / 0.0022046;
-  document.getElementById("outputGrams").innerHTML = valNum / 0.0022046;
-}
+// Calling the Inputs by ID
+let kgRef = document.getElementById("kg");
+let lbsRef = document.getElementById("lbs");
+let ozRef = document.getElementById("oz");
+
+let convertFromKg = () => {
+  let kg = kgRef.value;
+  //toFixed(2) limits the decimals to 2 digits.
+  lbsRef.value = (kg * 2.205).toFixed(2);
+  ozRef.value = (kg * 35.274).toFixed(2);
+};
+
+kgRef.addEventListener("input", convertFromKg);
