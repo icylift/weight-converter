a3da47646022f20e5de96e27c34afb19dd48eff4
Aaron Williams <polarizedcuriosity@gmail.com>
2023-09-03 15:32:23 -0700

    fixed the math for the weight conversion, and added some css to make it look better.

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 function weightConvert(value) {
-  document.getElementById("Kilograms").innerHTML = value * 2.2046;
-  document.getElementById("Ounces").innerHTML = value * 35.274;
+  document.getElementById("Kilograms").innerHTML = value / 2.2046;
+  document.getElementById("Ounces").innerHTML = value * 16;
 }
